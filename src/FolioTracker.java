b5b67f92dd95461f0bd15a6411b6c6dc77e19a53
Jack Bowden SE2016 <jack.bowden.2016@uni.strath.ac.uk>
2018-11-18 15:42:16 +0000

    updated foliotracker - now counts mins/max's - cleaned up variables - fixed exceptions in the strathQuoteServer

--- src/FolioTracker.java
+++ src/FolioTracker.java
@@ -5,8 +5,12 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class FolioTracker {
-	private static String sampleTicker;
+	private static Double sampleTicker;
 	private static List<String> stocks;
+	private static List<Double> stockValue;
+	private static List<Double> maxStockValue;
+	private static List<Double> minStockValue;
+	
 	private final ScheduledExecutorService scheduler =
 		     Executors.newScheduledThreadPool(1);
 	
@@ -16,11 +20,13 @@ public class FolioTracker {
 	
 	FolioTracker(){
 		init();
+		System.out.print(stockValue);			
 		scheduler.scheduleWithFixedDelay(new Runnable() {
 		    public void run() {
-		        System.out.println("scheduleWithFixedDelay: " + update());
+		        System.out.println("stocks: " + stocks + "\n" +  update());
 		    }
 		}, 5, 5 , TimeUnit.SECONDS);
+		
 	}
 			
 	private void init(){
@@ -31,19 +37,53 @@ public class FolioTracker {
 				add("AMZN");
 				add("ADS");
 			}
+			
+		};
+		stockValue = new ArrayList<Double>(){			
+			{
+				add(0.0);
+				add(0.0);
+				add(0.0);
+				add(0.0);
+			}
+		};
+		maxStockValue = new ArrayList<Double>(){			
+			{
+				add(0.0);
+				add(0.0);
+				add(0.0);
+				add(0.0);
+			}
+	};
+
+		minStockValue = new ArrayList<Double>(){			
+			{
+				add(0.0);
+				add(0.0);
+				add(0.0);
+				add(0.0);
+			}
 		};
 	}
 	
-	private List<String> update(){
+	private List<Double> update(){
 		try {
 			for(int i =0; i < stocks.size(); i++){
-				sampleTicker = StrathQuoteServer.getLastValue(stocks.get(i));
-				stocks.set(i, sampleTicker);
+				sampleTicker = Double.valueOf(StrathQuoteServer.getLastValue(stocks.get(i)));
+				stockValue.set(i, sampleTicker);								
+				// if new max, set new max
+				if(stockValue.get(i) > maxStockValue.get(i)){
+					maxStockValue.set(i, stockValue.get(i));
+				} // if new min, set new min
+				if(stockValue.get(i) < minStockValue.get(i)){
+					minStockValue.set(i, stockValue.get(i));
+				}
 			}
 		} catch (Exception e) {
-			System.err.println(e);
-			
-		}return stocks;
-	} 
-}
+			System.err.println(e);			
+		}
+		return stockValue;
+	}
 
+
+}
