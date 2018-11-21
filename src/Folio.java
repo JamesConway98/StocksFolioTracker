f78e133bc9915471b16c957c63efae7fddf95942
Jack Bowden SE2016 <jack.bowden.2016@uni.strath.ac.uk>
2018-11-21 16:17:26 +0000

    Folio changed - finished update stock - note update stock takes a List of tickers, which are converted using the getListTicker() - sets are difficult to get items from, its easier this way

--- src/Folio.java
+++ src/Folio.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Folio implements IFolio {
-	private Set<Stock> stocks;
+	private static Set<Stock> stocks;
+	private static List<String> tickers;
 	private String name;
-	private StrathQuoteServer server;
 	private double totalHolding;
+	
+	public static void main(String[] args) {
+		stocks = new HashSet<Stock>();
+		stocks.add(new Stock("AAPL", "stock_name", 220, 5));
+		tickers = getListTicker();
+		new Folio("hello").updateStock(tickers);
+	}
 	
 	Folio(String name){
 		this.name = name;
@@ -22,12 +31,10 @@ public class Folio implements IFolio {
 	public double totalHolding() {
 		return calcTotalHolding();
 	}
-
 	
 	public Set<Stock> getStocks() {
 		return stocks;
 	}
-
 	
 	public Stock getStock(String tickerSymbol) {
 		for(Stock s: stocks) {
@@ -36,16 +43,22 @@ public class Folio implements IFolio {
 	}
 
 	
-	public boolean addStock(String name, String ticker, double pricePerShare, int numShares) {
-		Stock x = new Stock(name, ticker, pricePerShare, numShares);
+	public boolean addStock(String symbol, String name, double value, int amount) {
+		Stock x = new Stock(symbol, name, value, amount);
 		return stocks.add(x);
 	}
-
 	
-	public boolean updateStock() {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean updateStock(List<String> tickers) {
+		new refreshData(tickers);
+		return true;
 	}
-
+	
+	private static List<String> getListTicker() {
+		List<String> tickerSymbols = new ArrayList<String>();
+		for(Stock s: stocks) {
+			tickerSymbols.add(s.getSymbol());
+			System.out.println("stock here: "  + s.getSymbol());
+		}return tickerSymbols;
+	}
 
 }
