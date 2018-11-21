1f1b6337f16ec605719e72976c0c5fa258c217d5
Jack Bowden SE2016 <jack.bowden.2016@uni.strath.ac.uk>
2018-11-21 14:36:49 +0000

    Upload New File

---
+++ src/IStock.java
@@ -0,0 +1,20 @@
+
+public interface IStock {
+
+	public String getTickerSymbol();
+	
+	public double getPricePerShare();
+	
+	public Boolean setPricePerShare(double pps);
+	
+	public int getNumOfShares();
+	
+	public Boolean setNumOfShares();
+	
+	public Double getValue();
+	
+	public Boolean getChange();
+	
+	
+	
+}
